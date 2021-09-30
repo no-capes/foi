35cccc98359b5a3cb455b333882a724de942c166
Prasuna Kunasani <gnana.kunasani@gmail.com>
2021-09-29 18:02:39 -0600

    FLAV-56 - Moved inline css and scripts from home page since it violates CSP. Added hashes related to ybug to csp tag

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -2,7 +2,9 @@
 var mybutton = document.getElementById("myBtn");
 
 // When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function () {
+    scrollFunction()
+};
 
 function scrollFunction() {
     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
@@ -17,3 +19,22 @@ function topFunction() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
 }
+
+
+$(document).ready(function () {
+    quoteSwiper.controller.control = imageSwiper;
+    imageSwiper.controller.control = quoteSwiper;
+});
+
+//Ybug code start
+
+(function () {
+    window.ybug_settings = {"id": "cpz575zzdkn9ny2sc1xj"};
+    var ybug = document.createElement('script');
+    ybug.type = 'text/javascript';
+    ybug.async = true;
+    ybug.src = 'https://widget.ybug.io/button/' + window.ybug_settings.id + '.js';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(ybug, s);
+})();
+//Ybug code end
