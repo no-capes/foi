f502afb6c7c16b79ac61c94fa5b3c290ecf4a0e5
Prasuna Kunasani <gnana.kunasani@gmail.com>
2021-09-30 12:24:15 -0600

    FLAV-56 - Fixed CSP issues on menu page

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -25,16 +25,3 @@ $(document).ready(function () {
     quoteSwiper.controller.control = imageSwiper;
     imageSwiper.controller.control = quoteSwiper;
 });
-
-//Ybug code start
-
-(function () {
-    window.ybug_settings = {"id": "cpz575zzdkn9ny2sc1xj"};
-    var ybug = document.createElement('script');
-    ybug.type = 'text/javascript';
-    ybug.async = true;
-    ybug.src = 'https://widget.ybug.io/button/' + window.ybug_settings.id + '.js';
-    var s = document.getElementsByTagName('script')[0];
-    s.parentNode.insertBefore(ybug, s);
-})();
-//Ybug code end
