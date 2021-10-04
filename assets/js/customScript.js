239c1e76ab8f220817091ad4668a92ce11939198
Prasuna Kunasani <gnana.kunasani@gmail.com>
2021-10-03 18:55:44 -0600

    FLAV-65 and FLAV-56 - CSP broke inline func call and the go to top button no longer worked. Fixed that and got rid of 'unsafe-hashes'.

--- assets/js/customScript.js
+++ assets/js/customScript.js
@@ -1,5 +1,5 @@
 //Get the button
-var mybutton = document.getElementById("myBtn");
+var mybutton = document.getElementById("goToTopBtn");
 
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function () {
@@ -15,11 +15,11 @@ function scrollFunction() {
 }
 
 // When the user clicks on the button, scroll to the top of the document
-function topFunction() {
+var goToTop = document.getElementById('goToTopBtn');
+goToTop.onclick = function(){
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
 }
-
 
 $(document).ready(function () {
     quoteSwiper.controller.control = imageSwiper;
